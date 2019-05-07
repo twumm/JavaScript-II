f2ee355c7f761be5c504b3cc7aa8e4a0f8d2a0cd
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-07 13:47:22 +0000

    chore: Complete counterFactory stretch goal

--- assignments/closure.js
+++ assignments/closure.js
@@ -34,4 +34,19 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 0;
+
+  const increment = () => {
+    let counter = 0;
+    return ++counter;
+  }
+
+  const decrement = () => --counter;
+  
+  return { increment, decrement }
 };
+
+// const newCounterFactory = counterFactory();
+// console.log(newCounterFactory.increment());
+// console.log(newCounterFactory.decrement());
+// console.log(newCounterFactory.decrement());
